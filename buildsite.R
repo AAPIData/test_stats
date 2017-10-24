031527576acb907c0f74768dd8ce980dea52bc7c
Sono <sonoshah@gmail.com>
2017-10-24 14:46:22 -0700

    Initial upload

---
+++ buildsite.R
@@ -0,0 +1,21 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("~/quick_stats/")
+#render your sweet site. 
+rmarkdown::render_site()
+
+setwd("~/quick_stats/")
+topics = c("Poverty","Nativity","Education","Insurance")
+
+for(topic in topics)
+  rmarkdown::render(input = "report_template.Rmd", 
+                    output_file = paste0("state/",topic,".html"),
+                    params = list(topic = topic)
+  )
+
+
+
+
+
