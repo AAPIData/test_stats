382f3fa0a019298608d08bf99da76b44f1812174
Sono Shah <sonoshah@gmail.com>
2017-11-22 10:32:46 -0800

    asd

--- buildsite.R
+++ buildsite.R
@@ -6,25 +6,25 @@ setwd("~/quick_stats/")
 #render your sweet site. 
 rmarkdown::render_site()
 
-setwd("~/quick_stats/")
-topics = c("Poverty","Nativity","Education","Insurance")
-
-for(topic in topics)
-  rmarkdown::render(input = "report_template.Rmd", 
-                    output_file = paste0("state/",topic,".html"),
-                    params = list(topic = topic)
-  )
-
-
-national_topics = c("Poverty","Nativity","Education","Insurance")
-
-for(topic in topics)
-  rmarkdown::render(input = "national_report_template.Rmd", 
-                    output_file = paste0("national/",national_topics,".html"),
-                    params = list(national_topics = national_topics)
-  )
-
-
-
-
-
+# setwd("~/quick_stats/")
+# topics = c("Poverty","Nativity","Education","Insurance")
+# 
+# for(topic in topics)
+#   rmarkdown::render(input = "report_template.Rmd", 
+#                     output_file = paste0("state/",topic,".html"),
+#                     params = list(topic = topic)
+#   )
+# 
+# 
+# national_topics = c("Poverty","Nativity","Education","Insurance")
+# 
+# for(topic in topics)
+#   rmarkdown::render(input = "national_report_template.Rmd", 
+#                     output_file = paste0("national/",national_topics,".html"),
+#                     params = list(national_topics = national_topics)
+#   )
+# 
+# 
+# 
+# 
+# 
